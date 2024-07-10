56b39fb7367ba761d8558a2376fe25dbe9a7180c
Marcel Mersch <110622172+MarcelMersch@users.noreply.github.com>
2024-07-10 13:48:33 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,22 +1,23 @@
 var lastActiveTab = 0;
+var lastActiveURL = 0;
 
 const map = new Map();
 
 //Ausführung, wenn der aktive Tab gewechselt wird
-chrome.tabs.onActivated.addListener(moveToFirstPosition);
+chrome.tabs.onActivated.addListener(datenSpeichernTabWechsel);
 
 //Ausführung, wenn die URL in einem Tab geändert wird
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.url) {
       console.log("Test1");
-      console.log("Test2");
+      datenSpeichernURLWechsel();
 
   }
 });
 
-async function moveToFirstPosition(activeInfo) {
+//Funktion, die URL und Dauer in Chrome speichert, wenn aktiver Tab gewechselt wird
+async function datenSpeichernTabWechsel() {
   try {
-    //await chrome.tabs.move(activeInfo.tabId, {index: 0});
        var zeitAktuell = Date.now();
        //TabID auslesen
        chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
@@ -42,10 +43,6 @@ async function moveToFirstPosition(activeInfo) {
             chrome.storage.local.set(obj).then(() => {
             });
 
-            //Entfernen???
-            chrome.storage.local.get([String(zeitAktuell)]).then((result) => {
-            });
-            
           }catch{
           console.log("Fehler beim Speichern der Zeit")
         }
@@ -57,14 +54,54 @@ async function moveToFirstPosition(activeInfo) {
 
   } catch (error) {
     if (error == "Error: Tabs cannot be edited right now (user may be dragging a tab).") {
-      setTimeout(() => moveToFirstPosition(activeInfo), 50);
+      setTimeout(() => datenSpeichernTabWechsel(), 50);
     } else {
     }
   }
 }
+//Funktion, die URL und Dauer in Chrome speichert, wenn URL im aktiven Tab gewechselt wird
+async function datenSpeichernURLWechsel() {
+  try {
+       var zeitAktuell = Date.now();
+       //TabID auslesen
+       chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        //Aktuelle Zeit im Dict speichern
+        map.set(tabs[0].url, [zeitAktuell, tabs[0].url]);
+        //Zeit ermitteln und Dauer vom alten Tab speichern
+        
+        //Startzeit vom letzten Tab ermittlen. Fehler, wenn es der 1. Tab der Fensters ist.
+        try{
+            var result = map.get(lastActiveURL);
+            console.log(result);
+            var startZeit = result[0];
+            var letzteURL  = result[1];
+            dauer = zeitAktuell - startZeit;
+            //Daten in Objekt speichern
+            var obj = {};       
+            var key = zeitAktuell;  
+            obj[key] = [letzteURL, dauer];
+            
+            console.log(letzteURL);
+            console.log(dauer);
+            chrome.storage.local.set(obj).then(() => {
+            });
+
+          }catch{
+          console.log("Fehler beim Speichern der Zeit")
+        }
+        //Nun die URL aktualisieren
+        lastActiveURL = tabs[0].url;
+
+      });
 
 
-//Part von Yannick
+  } catch (error) {
+    if (error == "Error: Tabs cannot be edited right now (user may be dragging a tab).") {
+      setTimeout(() => datenSpeichern(), 50);
+    } else {
+    }
+  }
+}
 
 let intervalId;
 
@@ -87,7 +124,6 @@ function resetAndStartTimer() {
         let startTime = result.startTime;
         let lastKnownTime = result.lastKnownTime;
 
-        // If lastKnownTime is not available or a large gap exists, reset the timer
         if (!lastKnownTime || (currentTime - lastKnownTime > 3600000)) {
             startTime = currentTime;
             chrome.storage.local.set({ startTime: startTime, timer: 0 });
@@ -99,8 +135,6 @@ function resetAndStartTimer() {
             const elapsedMilliseconds = Date.now() - startTime;
             const elapsedSeconds = Math.floor(elapsedMilliseconds / 1000);
             chrome.storage.local.set({ timer: elapsedSeconds, lastKnownTime: Date.now() });
-
-            //console.log(`Timer: ${elapsedSeconds} Sekunden`);
 
             if (elapsedSeconds >= 3600) {
                 chrome.notifications.create({
